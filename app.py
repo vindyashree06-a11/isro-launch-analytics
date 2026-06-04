fccb1115ff4c32334344fc4a10c57d18cbc8b9d1
vindyashree06-a11 <vindyashree06@gmail.com>
2026-06-04 11:43:12 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="ISRO Mission Analytics",
+    page_icon="🚀",
+    layout="wide"
+)
+
+st.title("🚀 ISRO Mission Analytics Dashboard")
+st.markdown("---")
+
+st.write(
+    "Comprehensive analytics of ISRO launches"
+)
